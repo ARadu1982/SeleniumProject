fdb878e866a3de382cf027357561bfaa5be978f2
Radu Constantin Amaritei <Radu.Amaritei@endava.com>
2024-12-08 18:29:51 +0200

    Continued test automation with PracticeForm

---
+++ src/test/java/Tests/PracticeFormTest.java
@@ -0,0 +1,57 @@
+package Tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class PracticeFormTest {
+
+    public WebDriver driver;
+
+    @Test
+    public void automationMethod() {
+
+        // Deschidem un browser de chrome
+        driver = new ChromeDriver();
+        //accesam o pagina web
+        driver.get("https://demoqa.com/");
+        //facem browserul in modul maximize
+        driver.manage().window().maximize();
+
+        //facem un scroll
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, document.body.scrollHeight)");
+
+        //declaram un element
+        WebElement formElement = driver.findElement(By.xpath("//h5[text()='Forms']"));
+        formElement.click();
+
+        WebElement practiceFormElement = driver.findElement(By.xpath("//span[text()='Practice Form']"));
+        practiceFormElement.click();
+
+        WebElement firstNameElement = driver.findElement(By.id("firstName"));
+        String firstNameValue = "Amaritei";
+        firstNameElement.sendKeys(firstNameValue);
+
+        WebElement lastNameElement = driver.findElement(By.id("lastName"));
+        String lastNameValue = "Radu";
+        lastNameElement.sendKeys(lastNameValue);
+
+        WebElement userEmailElement = driver.findElement(By.id("userEmail"));
+        String userEmailValue = "radu@yahoo.com";
+        userEmailElement.sendKeys(userEmailValue);
+
+        WebElement mobileNumberElement = driver.findElement(By.cssSelector("input[placeholder='Mobile Number']"));
+        String mobileNumberValue = "23232323";
+        mobileNumberElement.sendKeys(mobileNumberValue);
+
+        WebElement pictureElement = driver.findElement(By.id("uploadPicture"));
+        File file = new File("src/test/resources/Capture1.PNG");
+        pictureElement.sendKeys(file.getAbsolutePath());
+    }
+}
